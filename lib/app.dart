4b26786d0a03d36d573ab4a1e18afc6ce159a96b
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2021-08-24 13:43:41 +0200

    Improved the website name and icons

--- lib/app.dart
+++ lib/app.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Desmos mainnet balance verifier',
       theme: ThemeData(
         fontFamily: 'RaleWay',
         primarySwatch: MaterialColor(
